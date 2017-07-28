5153775f05e013de3e4ef19c39ce7ceb18bbbfd3
Albilaga Linggra Pradana <albilaga.lp@gmail.com>
2017-07-28 11:17:23 +0700

    testing login
    testing login

--- Droid/Views/LoginView.cs
+++ Droid/Views/LoginView.cs
@@ -1,26 +1,41 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Views;
+using Android.Support.Design.Widget;
 using Android.Widget;
+using ReactiveUI;
+using ReactiveUITest.ViewModels;
 
 namespace ReactiveUITest.Droid.Views
 {
-    [Activity(Label = "LoginView")]
-    public class LoginView : Activity
+    [Activity(Label = "LoginView", MainLauncher = true)]
+    public class LoginView : ReactiveActivity<LoginViewModel>
     {
+        public LoginView()
+        {
+            this.WhenActivated(registerDisposable =>
+            {
+                registerDisposable(this.Bind(this.ViewModel, x => x.Username, x => x.Username.Text));
+                registerDisposable(this.Bind(this.ViewModel, x => x.Password, x => x.Password.Text));
+                registerDisposable(this.BindCommand(this.ViewModel, x => x.Login, x => x.LoginButton));
+                registerDisposable(this.BindCommand(this.ViewModel, x => x.Reset, x => x.Reset, nameof(Reset.Click)));
+            });
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
+            SetContentView(Resource.Layout.LoginLayout);
+
+            this.WireUpControls();
         }
+
+        #region Properties
+        public TextInputEditText Username { get; private set; }
+        public TextInputEditText Password { get; private set; }
+        public Button LoginButton { get; private set; }
+        public TextView Reset { get; private set; }
+        #endregion
     }
 }
--- ReactiveUITest/ViewModels/LoginViewModel.cs
+++ ReactiveUITest/ViewModels/LoginViewModel.cs
@@ -1,10 +1,69 @@
 ﻿using System;
+using ReactiveUI;
+using System.Reactive;
+using System.Reactive.Linq;
+
 namespace ReactiveUITest.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : ReactiveObject
     {
+
         public LoginViewModel()
         {
+            var canLogin = this.WhenAnyValue(
+                x => x.Username, x => x.Password, (user, pass)
+                => !string.IsNullOrWhiteSpace(user) && string.IsNullOrEmpty(pass)
+                && user.Length >= 6 && pass.Length >= 6);
+            this._Login = ReactiveCommand.CreateFromObservable(this.LoginAsync, canLogin);
+            this._Reset = ReactiveCommand.Create(() =>
+            {
+                this.Username = null;
+                this.Password = null;
+            });
         }
+
+        #region Properties
+        private string _Username;
+        public string Username
+        {
+            get { return this._Username; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref this._Username, value);
+            }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get { return this._Password; }
+            set { this.RaiseAndSetIfChanged(ref this._Password, value); }
+        }
+        #endregion
+
+        #region Commands
+        private readonly ReactiveCommand<Unit, Unit> _Login;
+        public ReactiveCommand<Unit, Unit> Login => this._Login;
+
+        private readonly ReactiveCommand<Unit, Unit> _Reset;
+        public ReactiveCommand<Unit, Unit> Reset => this._Reset;
+        #endregion
+
+        #region Private Methods
+        private IObservable<Unit> LoginAsync() =>
+       Observable
+           .Return(new Random().Next(0, 2) == 1)
+           .Delay(TimeSpan.FromSeconds(1))
+           .Do(
+               success =>
+               {
+                   if (!success)
+                   {
+                       throw new InvalidOperationException("Failed to login.");
+                   }
+               }
+           )
+            .Select((arg) => Unit.Default);
+        #endregion
     }
 }
